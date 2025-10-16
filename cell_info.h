5e039f20ff4a43fe841564cba01f0f75248fd38b
Audrius Puplinskas <audrius@ipk.lt>
2025-10-16 22:07:03 +0300

    Add files via upload

---
+++ cell_info.h
@@ -0,0 +1,27 @@
+#ifndef _VRE_APP_WIZARDTEMPLATE_
+#define	_VRE_APP_WIZARDTEMPLATE_
+
+#include "vmsys.h"
+#include "vmio.h"
+#include "vmgraph.h"
+#include "vmchset.h"
+#include "vmstdlib.h"
+#include "vmcell.h"
+#include "vmtimer.h"
+#include "string.h"
+#include <time.h>
+#include "console.h"
+
+VMINT layer_hdl[1];
+
+void handle_sysevt(VMINT message, VMINT param);
+void handle_keyevt(VMINT event, VMINT keycode);
+void timer(int a);
+void create_app_txt_path(VMWSTR text, VMSTR extt);
+void checkFileExist(void);
+static void fill_screen(void);
+void create_auto_full_path_name(VMWSTR result, VMWSTR fname);
+void create_app_txt_filenamex(VMWSTR text, VMSTR extt);
+
+#endif
+
